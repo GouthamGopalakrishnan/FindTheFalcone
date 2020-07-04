a6e8a9416fc610204d09b7bbff1cdde49e528f30
Goutham G <gauthamg1999@gmail.com>
2020-07-04 17:04:27 +0530

    Updates

---
+++ precache-manifest.f36bcadddf206ba95b986791db7c2ed5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e820aac3ef76a2206a676b25f8089095",
+    "url": "/FindTheFalcone/index.html"
+  },
+  {
+    "revision": "0d7371ab58907214b1c1",
+    "url": "/FindTheFalcone/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "38b548ef26b6810752c6",
+    "url": "/FindTheFalcone/static/css/main.6655fc7a.chunk.css"
+  },
+  {
+    "revision": "0d7371ab58907214b1c1",
+    "url": "/FindTheFalcone/static/js/2.e5504b0e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FindTheFalcone/static/js/2.e5504b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b548ef26b6810752c6",
+    "url": "/FindTheFalcone/static/js/main.bd6dc98b.chunk.js"
+  },
+  {
+    "revision": "ac493ccf5eff94530673",
+    "url": "/FindTheFalcone/static/js/runtime-main.611c5616.js"
+  },
+  {
+    "revision": "7e244568918b596817c367c834cacbed",
+    "url": "/FindTheFalcone/static/media/Logo.7e244568.png"
+  },
+  {
+    "revision": "db7e644defab14691ac5d9b0d61fa001",
+    "url": "/FindTheFalcone/static/media/back.db7e644d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FindTheFalcone/precache-manifest.df34ef33d647c071e49a24505da29eed.js"
+  "/FindTheFalcone/precache-manifest.f36bcadddf206ba95b986791db7c2ed5.js"
 );
 
 self.addEventListener('message', (event) => {
